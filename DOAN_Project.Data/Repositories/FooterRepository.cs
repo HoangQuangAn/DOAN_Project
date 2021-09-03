c021b06e4e0a938628a2ce3fb77fb63aa46f37b9
Nelson An <hoangquangan63@gmail.com>
2021-09-03 14:59:00 +0700

    complete repository

---
+++ DOAN_Project.Data/Repositories/FooterRepository.cs
@@ -0,0 +1,17 @@
+﻿using DOAN_Project.Data.Infrastructure;
+using DOAN_Project.Model.Models;
+using DOAN_Project.Model.Repositories;
+
+namespace DOAN_Project.Data.Repositories
+{
+    public interface IFooterRepository
+    {
+    }
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
