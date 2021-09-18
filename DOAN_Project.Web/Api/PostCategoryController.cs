aaac00753b509346e8ac2ebd3a94a9c31b47bcbc
Nelson An <hoangquangan63@gmail.com>
2021-09-18 13:20:07 +0700

    how to use web APi

---
+++ DOAN_Project.Web/Api/PostCategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DOAN_Project.Web.Api
+{
+    public class PostCategoryController : ApiController
+    {
+    }
+}
