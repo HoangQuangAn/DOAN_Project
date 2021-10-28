84e244f0cac212019bfdbe152fcc17935ed61932
Nelson An <hoangquangan63@gmail.com>
2021-10-28 17:39:12 +0700

    add automapper for web

---
+++ DOAN_Project.Web/Models/PostViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DOAN_Project.Web.Models
+{
+    public class PostViewModel
+    {
+        public int ID { set; get; }
+
+        public string Name { set; get; }
+
+        public string Alias { set; get; }
+
+        public int CategoryID { set; get; }
+
+        public string Image { set; get; }
+
+        public string Description { set; get; }
+
+        public string Content { set; get; }
+
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
+
+        public virtual PostCategoryViewModel PostCategory { set; get; }
+        public IEnumerable<PostTagViewModel> PostTags { get; set; }
+        public DateTime? CreatedDate { set; get; }
+
+        public string CreatedBy { set; get; }
+        public DateTime? UpdatedDate { set; get; }
+
+        public string UpdateBy { set; get; }
+
+        public string MetaKeyword { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
